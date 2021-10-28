e339c2e25c0ab9c6d3a030728c8113f1fd233185
NirobNabil <thetwin11100@gmail.com>
2021-10-28 23:08:44 +0600

    editable font

--- qr.py
+++ qr.py
@@ -4,6 +4,20 @@ from qrcode.image import styledpil
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
+from wand import font
+from wand.image import Image as WandImage
+from wand.font import Font
+from wand.display import display
+
+with WandImage() as img:
+    img.background_color = 'white'
+    img.font = Font('caveat.ttf', 60)  
+    img.read(filename='label: SCAN HERE                                                                         SCAN HERE                                                     SCAN HERE                                                    ')
+    img.virtual_pixel = 'white'
+    # 360 degree arc, rotated -90 degrees
+    img.distort('arc', (360,-86))
+    img.save(filename='text.png')
+    img.format = 'png'
 
 qr = qrcode.QRCode(
     version=2,
@@ -20,7 +34,7 @@ sx, sy = img_3.size
 copy_region = img_3.crop( ( int(sx/4)*2, 0, int(sy/4)*3, sy ) )
 csx, csy = copy_region.size
 
-img_3 = qr.make_image(image_factory=StyledPilImage, embeded_image_path="logo.png", module_drawer=RoundedModuleDrawer())
+img_3 = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer())
 
 new_im = Image.new('RGB', (sx*3, sy*3), (255,255,255))
 for ix in range(12):
@@ -30,13 +44,31 @@ new_im.paste(img_3, (csx*4,csy*1))
 x = new_im.size[0] / 6
 new_im = new_im.crop(  ( x, x, x*5, x*5 ) )
 
-template = Image.open('template.png')
+
+template = Image.open('template_final.png')
 template = template.resize(new_im.size)
 mask = Image.open('template_mask.png')
 mask = mask.resize(new_im.size)
 mask = mask.convert('L')
-
 new_im = Image.composite(template, new_im, mask)
+
+white = Image.open('white.png')
+white = white.resize(new_im.size)
+text_color = white.copy()
+mask = white.copy()
+font_fix = 80
+mask.paste( Image.open('text.png').resize( (white.size[0]-font_fix, white.size[1]-font_fix) ), (int(font_fix/2),int(font_fix/2)) )
+mask = mask.resize(new_im.size)
+mask = mask.convert('L')
+text_color.save("gg.png")
+new_im = Image.composite(new_im, white, mask)
+
+logo = Image.open('logo.png')
+size = 300, 300
+logo.thumbnail(size, Image.ANTIALIAS)
+new_im.paste( logo, (int(new_im.size[0]/2) - int(logo.size[0]/2), int(new_im.size[1]/8)*7 - int(logo.size[1]/2) ), logo )
+
+
 new_im.save("temp.png")
 
 img_3.paste(  copy_region, (sx, 0) , copy_region  )
